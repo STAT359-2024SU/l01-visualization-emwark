d656ad3390f78e2e1cdd4676e4b937b127ea255a
emwark <emilywarkentin2029@u.northwestern.edu>
2025-06-29 21:37:52 -0500

    Finalize Ex 3

--- Warkentin_Emily_L01.R
+++ Warkentin_Emily_L01.R
@@ -12,10 +12,11 @@ tinder_data <- read_csv("data/tinder_data.csv")
 
 # Ex 1
 
+#definitions of geom and aesthetic mapping
 
 
 # Ex 2
-# scatterplot of 'hwy' v 'cty' using mp
+# scatterplot of 'hwy' v 'cty' using mpg set
 
 ggplot(data  = mpg,
        mapping = aes( x = hwy, y = cty)) +
@@ -30,3 +31,24 @@ ggplot(data  = mpg,
        mapping = aes( x = hwy, y = cty)) +
   geom_jitter(alpha = 0.2)
 # geom_jitter instead of geom_point 
+
+# Ex 3
+#scatterplot of 'hwy' v 'cty' with color of points to 'drv'
+ggplot(mpg, aes(x = hwy, y = cty, color = drv)
+) +
+  geom_point ()
+ggplot(mpg, aes(x = hwy, y = cty, color = drv)
+) +
+  geom_jitter(alpha = 0.2)
+# scatterplot of `hwy` versus `cty` w/ color of the points favorite color 
+# `facet` by `drv`. Read `?facet_wrap` and adjust the `ncol` and `scales` as necessary.
+
+ggplot(mpg, aes(x = hwy, y = cty)
+) +
+  geom_point(color = "slateblue") +
+  facet_wrap(~drv)
+
+ggplot(mpg, aes(x = hwy, y = cty)
+) +
+  geom_jitter(alpha = 0.2, color = "slateblue") +
+  facet_wrap(~drv)
