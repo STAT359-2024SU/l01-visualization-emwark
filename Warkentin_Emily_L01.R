3716bf43382d30df3e3d3cf0b72a72b3dbdac882
emwark <emilywarkentin2029@u.northwestern.edu>
2025-06-29 21:23:26 -0500

    Finalized Exercise #2

--- Warkentin_Emily_L01.R
+++ Warkentin_Emily_L01.R
@@ -1,10 +1,32 @@
 # L01-visualization ----
+# Stat
 
 # load packages and data --------------------------------------------------
+library(tidyverse)
 
+tinder_data <- read_csv("data/tinder_data.csv")
 
 
 # Exercises ---------------------------------------------------------------
 
 
 # Ex 1
+
+
+
+# Ex 2
+# scatterplot of 'hwy' v 'cty' using mp
+
+ggplot(data  = mpg,
+       mapping = aes( x = hwy, y = cty)) +
+  geom_point()
+
+ggplot(data  = mpg,
+       mapping = aes( x = hwy, y = cty)) +
+  geom_point(alpha = 0.2)
+# alpha attempt to solve overplotting issues
+
+ggplot(data  = mpg,
+       mapping = aes( x = hwy, y = cty)) +
+  geom_jitter(alpha = 0.2)
+# geom_jitter instead of geom_point 
